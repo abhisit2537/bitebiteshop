c0090e8b056c14f47fa3f514dc518a8b95bbce2b
abhisit snk <abhisit2537@hotmail.com>
2017-12-14 17:39:05 +0700

    fab font awsome commit

--- src/pages/shop/shop.model.ts
+++ src/pages/shop/shop.model.ts
@@ -1,50 +1,50 @@
 export class shopModel {
-    _id: String;
-    name: String;
-    name_eng: String;
-    detail: String;
+    _id: string;
+    name: string;
+    name_eng: string;
+    detail: string;
     address: Array<addressModel>;
-    tel: String;
-    email: String;
-    facebook: String;
-    line: String;
+    tel: string;
+    email: string;
+    facebook: string;
+    line: string;
     promoteimage: Array<any>
-    coverimage: String;
-    isactiveshop: String;
-    issendmail: String;
-    importform: String;
+    coverimage: string;
+    isactiveshop: string;
+    issendmail: string;
+    importform: string;
     times: Array<timeModel>;
 
 }
 export class addressModel {
-    addressdetail: String;
-    address: String;
-    subdistinct: String;
-    distinct: String;
-    province: String;
+    addressdetail: string;
+    address: string;
+    subdistinct: string;
+    distinct: string;
+    province: string;
     postcode: Number;
-    lat: String;
-    lng: String;
+    lat: string;
+    lng: string;
 }
 export class itemsModel {
     cate: Array<cateModel>
     products: Array<productsModel>
 }
 export class cateModel {
-    _id: String;
-    name: String;
-    image: String;
+    _id: string;
+    name: string;
+    image: string;
 }
 
 export class productsModel {
-    _id: String;
-    name: String;
-    image: String;
+    _id: string;
+    name: string;
+    image: string;
     price: Number;
 }
 export class timeModel{
-    description: String;
-    timestart: String;
-    timeend: String;
+    description: string;
+    timestart: string;
+    timeend: string;
     days: Array<any>;
 }
