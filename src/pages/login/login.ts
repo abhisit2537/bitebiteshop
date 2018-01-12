af4fa232755e7b36c1c4dd8bc9738e61b27d918d
mob35 <toonelicious@gmail.com>
2018-01-13 00:35:30 +0700

    fix roles

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -27,7 +27,7 @@ export class LoginPage {
     let loading = this.loading.create();
     loading.present()
     this.auth.login(this.credentials).then((res) => {
-      alert(JSON.stringify(res.roles.indexOf('shop')));
+      // alert(JSON.stringify(res.roles.indexOf('shop')));
       if (res.roles.indexOf('shop') >= 0) {
         window.localStorage.setItem('bikebikeshop', JSON.stringify(res));
         loading.dismiss();
