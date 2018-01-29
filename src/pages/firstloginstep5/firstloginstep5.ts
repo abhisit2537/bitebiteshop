4b39ea69760c7985a984c04924ff0e645ab37cb8
Bu4y <amnes1a@windowslive.com>
2018-01-29 13:18:45 +0700

    map

--- src/pages/firstloginstep5/firstloginstep5.ts
+++ src/pages/firstloginstep5/firstloginstep5.ts
@@ -55,15 +55,18 @@ export class Firstloginstep5Page {
   }
 
   ionViewWillEnter() {
-    let loadingCtrl = this.loading.create();
-    loadingCtrl.present();
+    // let loadingCtrl = this.loading.create();
+    // loadingCtrl.present();
     this.firstLogin = JSON.parse(window.localStorage.getItem('firstlogin'));
-    if (this.firstLogin.address.lat && this.firstLogin.address.lng) {
-      this.showMap();
-      loadingCtrl.dismiss();
-    } else {
-      loadingCtrl.dismiss();
-    }
+    this.address = window.localStorage.getItem('shop_location_address') ? JSON.parse(window.localStorage.getItem('shop_location_address')) : this.firstLogin.address;
+    // alert(JSON.stringify(this.address));
+    this.firstLogin.address = this.address ? this.address : this.firstLogin.address;
+    // if (this.firstLogin.address.lat && this.firstLogin.address.lng) {
+    // this.showMap();
+    // loadingCtrl.dismiss();
+    // } else {
+    //   loadingCtrl.dismiss();
+    // }
   }
   ionViewWillLeave() {
     console.log('ionViewWillLeave');
@@ -77,7 +80,8 @@ export class Firstloginstep5Page {
     console.log(this.firstLogin);
     this.shopServiceProvider.addFirstShop(this.firstLogin).then((data) => {
       loadingCtrl.dismiss();
-      localStorage.removeItem('firstlogin');
+      window.localStorage.removeItem('shop_location_address');
+      window.localStorage.removeItem('firstlogin');
       this.navCtrl.setRoot('TabnavPage');
     }, (err) => {
       loadingCtrl.dismiss();
@@ -90,78 +94,18 @@ export class Firstloginstep5Page {
     window.localStorage.setItem('firstlogin', JSON.stringify(this.firstLogin));
     this.navCtrl.setRoot('Firstloginstep1Page');
   }
-  getlocation() {
-    this.geolocation.getCurrentPosition().then((resp) => {
-      console.log(resp.coords.latitude + '    ' + resp.coords.longitude);
-      this.firstLogin.address.lat = resp.coords.latitude;
-      this.firstLogin.address.lng = resp.coords.longitude;
+  // getlocation() {
+  //   this.geolocation.getCurrentPosition().then((resp) => {
+  //     console.log(resp.coords.latitude + '    ' + resp.coords.longitude);
+  //     this.firstLogin.address.lat = resp.coords.latitude;
+  //     this.firstLogin.address.lng = resp.coords.longitude;
 
-    }).catch((error) => {
-      console.log('Error getting location', error);
-    });
-  }
+  //   }).catch((error) => {
+  //     console.log('Error getting location', error);
+  //   });
+  // }
   showMap() {
-
-    this.nativeGeocoder.reverseGeocode(this.firstLogin.address.lat, this.firstLogin.address.lng)
-      .then((result: NativeGeocoderReverseResult) => {
-        // alert(JSON.stringify(result))
-        // console.log(result);
-        this.address.address = result.subThoroughfare;
-        this.address.subdistinct = result.locality;
-        this.address.distinct = result.subAdministrativeArea;
-        this.address.province = result.administrativeArea;
-        this.address.postcode = result.postalCode;
-
-      })
-      .catch((error: any) => console.log(error));
-
-    let mapOptions: GoogleMapOptions = {
-      camera: {
-        target: {
-          lat: this.firstLogin.address.lat,
-          lng: this.firstLogin.address.lng
-        },
-        zoom: 18,
-        tilt: 30
-      }
-    };
-    let loadingCtrl = this.loading.create();
-    loadingCtrl.present();
-    let mapref = this.mapref.nativeElement;
-    this.map = this.googleMaps.create(mapref, mapOptions);
-    // alert('Map!');
-
-    // Wait the MAP_READY before using any methods.
-    this.map.one(GoogleMapsEvent.MAP_READY)
-      .then(() => {
-        // alert('Map is ready!');
-
-        // Now you can use all methods safely.
-        this.map.addMarker({
-          icon: 'blue',
-          animation: 'DROP',
-          position: {
-            lat: this.firstLogin.address.lat,
-            lng: this.firstLogin.address.lng
-          }
-        })
-          .then(marker => {
-            marker.on(GoogleMapsEvent.MARKER_CLICK)
-              .subscribe(() => {
-                this.nativeGeocoder.reverseGeocode(this.firstLogin.address.lat, this.firstLogin.address.lng)
-                  .then((result: NativeGeocoderReverseResult) => {
-                    // alert(JSON.stringify(result))
-                    this.address.address = result.subThoroughfare;
-                    this.address.subdistinct = result.locality;
-                    this.address.distinct = result.subAdministrativeArea;
-                    this.address.province = result.administrativeArea;
-                    this.address.postcode = result.postalCode;
-                  })
-                  .catch((error: any) => console.log(error));
-              });
-          });
-      });
-    loadingCtrl.dismiss();
+    this.navCtrl.push('GoogleMapsPage');
   }
 }
 
