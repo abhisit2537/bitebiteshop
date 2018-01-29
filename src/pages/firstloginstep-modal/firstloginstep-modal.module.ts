d20e151fd0256890dc244c45bfd1a6125e2ecea9
abhisit snk <abhisit2537@hotmail.com>
2018-01-29 18:35:04 +0700

    translate

--- src/pages/firstloginstep-modal/firstloginstep-modal.module.ts
+++ src/pages/firstloginstep-modal/firstloginstep-modal.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { FirstloginstepModalPage } from './firstloginstep-modal';
+import { TranslateModule } from '@ngx-translate/core';
+
 
 @NgModule({
   declarations: [
@@ -8,6 +10,7 @@ import { FirstloginstepModalPage } from './firstloginstep-modal';
   ],
   imports: [
     IonicPageModule.forChild(FirstloginstepModalPage),
+    TranslateModule.forChild()
   ],
 })
 export class FirstloginstepModalPageModule {}
